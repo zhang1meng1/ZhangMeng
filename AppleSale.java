32c953d14318a95da445e91fd549f19d1136ed6d
zhang1meng1 <71574992+zhang1meng1@users.noreply.github.com>
2020-09-20 09:38:36 +0800

    Add files via upload

---
+++ AppleSale.java
@@ -0,0 +1,15 @@
+package ketang;
+public class AppleSale {
+	public static void main(String[]args){
+		int before = 20;  //java中默认浮点数类型是doule，如果需要声明float类型，则要在数字后面加F
+		double rise = 9.8;//float rise = 9.8F;
+		
+						  //int与double运算结果为——double
+						  //double result = before + rise;
+		
+		//大数据范围的结果—— 小数据范围的变量来接受+（小范围数值类型)
+						  //强制转换 问题：精度缺失
+		int result = before + (int)rise;
+		System.out.print("今年苹果笔记本的销售额为:"+ result);
+	}
+}
